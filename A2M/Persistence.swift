63674db9691f2ca1079b48d0f6d4d98587ca9f41
alex <alexleung9407@gmail.com>
2023-05-25 17:32:43 +1000

    persistent update

--- A2M/Persistence.swift
+++ A2M/Persistence.swift
@@ -1,28 +1,15 @@
-import Foundation
 import CoreData
 
-struct Persistence {
-    static var shared = Persistence()
-    let container: NSPersistentContainer
+class PersistentStorageController {
+    static let shared = PersistentStorageController()
 
-    init() {
-        container = NSPersistentContainer(name: "FavouritePlaces")
-        container.loadPersistentStores { _, error in
-            if let error = error {
-                fatalError("Error loading data, \(error)")
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "A2M")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        }
-    }
-
-    func save() {
-        let context = container.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
+        })
+        return container
+    }()
 }
