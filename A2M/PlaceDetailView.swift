5d7ab1afda812a291cbc343149f2c9ed37d92f6d
alex <alexleung9407@gmail.com>
2023-05-31 14:42:55 +1000

    photo

--- A2M/PlaceDetailView.swift
+++ A2M/PlaceDetailView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import MapKit
 
 struct PlaceDetailView: View {
     @Environment(\.managedObjectContext) private var viewContext
@@ -8,37 +9,47 @@ struct PlaceDetailView: View {
     @State private var editedName: String = ""
     @State private var editedLocation: String = ""
     @State private var editedNotes: String = ""
+    @State private var imageURL: String = ""
     
-    @State private var updatedPlace: Place // Add this line
+    @State private var updatedPlace: Place
     
     init(place: Place, viewModel: PlaceViewModel) {
         self.place = place
         self.viewModel = viewModel
-        self._updatedPlace = State(initialValue: place) // Add this line
+        self._updatedPlace = State(initialValue: place)
+        self._imageURL = State(initialValue: place.imageData ?? "")
     }
     
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .leading, spacing: 10) {
-                Text(updatedPlace.name ?? "") // Update this line
+                Text(updatedPlace.name ?? "")
                     .font(.title)
                     .fontWeight(.bold)
                     .frame(maxWidth: .infinity, alignment: .leading)
                 if !isEditMode {
                     VStack(alignment: .leading, spacing: 10) {
-                        Text(updatedPlace.location ?? "") // Update this line
-                            .padding(.bottom)
+
+                        if let imageDataString = place.imageData,
+                           let imageData = Data(base64Encoded: imageDataString),
+                           let image = UIImage(data: imageData) {
+                            Image(uiImage: image)
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        } else {
+                            Color.gray
+                        }
                         Text("Description")
                             .font(.title)
                             .fontWeight(.bold)
                             .padding(.bottom)
-                        Text(updatedPlace.notes ?? "") // Update this line
+                        Text(updatedPlace.notes ?? "")
                             .padding(.bottom)
                     }
-                    NavigationLink(destination: MapEditorView(model: MyLocation.shared, place: $updatedPlace, viewModel: viewModel)) { // Update this line
+                    NavigationLink(destination: MapEditorView(model: MyLocation.shared, place: $updatedPlace, viewModel: viewModel)) {
                         VStack {
-                            Text("Map of \(updatedPlace.name ?? "")") // Update this line
-                                .font(.title)
+                            Text("Map of \(updatedPlace.name ?? "")")
                                 .fontWeight(.bold)
                             Text("\(updatedPlace.latitude)")
                             Text("\(updatedPlace.longitude)")
@@ -47,8 +58,7 @@ struct PlaceDetailView: View {
                 } else {
                     VStack(alignment: .leading, spacing: 10) {
                         LabelTextField(label: "Name", placeHolder: "Fill in the location", text: $editedName)
-                        // Add your image view here
-                        LabelTextField(label: "Image", placeHolder: "Fill in the url", text: $editedLocation)
+                        LabelTextField(label: "Image", placeHolder: "Fill in the URL", text: $imageURL)
                         LabelTextField(label: "Notes", placeHolder: "Fill in the description", text: $editedNotes)
                     }
                 }
@@ -61,31 +71,36 @@ struct PlaceDetailView: View {
             Button(action: {
                 if isEditMode {
                     savePlace()
-                    isEditMode.toggle()
-                } else {
-                    isEditMode.toggle()
                 }
+                isEditMode.toggle() // Toggle the edit mode state
             }) {
                 Text(isEditMode ? "Done" : "Edit")
             }
         })
         .onAppear {
-            editedName = updatedPlace.name ?? "" // Update this line
+            editedName = updatedPlace.name ?? ""
             editedLocation = updatedPlace.location ?? ""
             editedNotes = updatedPlace.notes ?? ""
+            imageURL = updatedPlace.imageData ?? ""
         }
-        .onAppear {
-                    // Refresh the view and update the properties when it appears again
-                    editedName = place.name ?? ""
-                    editedLocation = place.location ?? ""
-                    editedNotes = place.notes ?? ""
-                }
+        .onChange(of: imageURL) { _ in
+            updateImage()
+        }
     }
     
     private func savePlace() {
-        updatedPlace.name = editedName // Update this line
-        updatedPlace.location = editedLocation // Update this line
-        updatedPlace.notes = editedNotes // Update this line
-        viewModel.savePlace(place: updatedPlace) // Update this line
+        updatedPlace.name = editedName
+        updatedPlace.location = editedLocation
+        updatedPlace.notes = editedNotes
+        viewModel.savePlace(place: updatedPlace)
+    }
+    
+    private func updateImage() {
+        if let url = URL(string: imageURL),
+           let imageData = try? Data(contentsOf: url) {
+            let base64String = imageData.base64EncodedString()
+            updatedPlace.imageData = base64String
+            viewModel.savePlace(place: updatedPlace)
+        }
     }
 }
